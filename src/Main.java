9b8236fc1ed21099715a2d73e651bbda54aa814f
02jos004 <02jos004@LK-S21574.lulea.se>
2020-01-26 00:15:11 +0100

    separate player choice from doTurn

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	static ArrayList<Card> deck = new ArrayList<Card>();
 	
 	static ArrayList<ArrayList<Card>> mainPiles = new ArrayList<ArrayList<Card>>();
-	static ArrayList<String> info = new ArrayList<String>();
+	//static ArrayList<String> info = new ArrayList<String>();
 	
 	static ArrayList<Card> tempf = null;
 	static ArrayList<Card> tempt = null;
@@ -87,59 +87,7 @@ public class Main {
 			System.out.println("Player "+currPlayer+" got a empty hand"); 
 		}
 		if(plist[currPlayer].ai == false) {
-		String c2="";
-		int choice = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("what do you want to do? 1. play card 2. show all known cards"));
-		a1:
-		if(choice==1) { // play card
-			String c1 = javax.swing.JOptionPane.showInputDialog("from hand/pile? c to cancel \n pile0-pile4 type the pile Number");
-			if(c1.equals("c")) {
-				break a1;
-			} else if(c1.equals("hand")) {
-				tempf = plist[0].hand;
-				c2 = javax.swing.JOptionPane.showInputDialog("position? 0-4, c to cancel ");
-				if(c2.equals("c")) {
-					break a1;
-				} else {
-					temppos = Integer.valueOf(c2);
-				}
-			} else if(Integer.valueOf(c1) != null) {
-				if(plist[0].pPiles.get(Integer.valueOf(c1)).size() != 0) {
-					tempf=plist[0].pPiles.get(Integer.valueOf(c1));
-				} else {
-					javax.swing.JOptionPane.showMessageDialog(null, "cant take from empty pile");
-					break a1;
-				}
-				
-			}
-			if(!(c1.equals("hand"))) {
-				temppos = tempf.size()-1;
-			}
-			if(c1.equals("c")) {
-				break a1;
-			}
-			String c3 =javax.swing.JOptionPane.showInputDialog("to pile? c to cancel \n mainpile0-mainpile3 pPile4-pPile7  type the pile number");
-			if(c3.equals("c")) {
-				break a1;
-			} else if(Integer.valueOf(c3) != null) {
-				if(Integer.valueOf(c3) > 3) {
-					tempt=plist[0].pPiles.get(Integer.valueOf(c3)-3);
-				} else {
-					tempt=mainPiles.get(Integer.valueOf(c3));
-				}
-			}
-			if(checkMove(tempf, temppos, tempt)) {
-				plist[0].playCard(tempf, temppos, tempt);
-				if(isToMainPile(tempt)) {
-					if(tempt.get(tempt.size()-1).id == 0) {
-						tempt.get(tempt.size()-1).id = tempt.size();
-					}
-				}
-			} else {
-				javax.swing.JOptionPane.showMessageDialog(null, "ILLEGAL MOVE");
-				doTurn();
-			}
-		} else if(choice==2) printBoard();
-		
+			playerChoice();
 		} else {
 			randomAi();
 			plist[currPlayer].playCard(tempf, temppos, tempt);
@@ -148,7 +96,7 @@ public class Main {
 					tempt.get(tempt.size()-1).id = tempt.size();
 				}
 			}
-			//printBoard();
+			printBoard();
 		}
 		if(tempt == plist[currPlayer].pPiles.get(1) || tempt == plist[currPlayer].pPiles.get(2) || tempt == plist[currPlayer].pPiles.get(3) || tempt == plist[currPlayer].pPiles.get(4)) roundEnd = true;;
 	}
@@ -161,7 +109,6 @@ public class Main {
 		}
 	}
 	static void endTurn() {
-		//roundEnd = true;
 		System.out.println("endturn");
 		currPlayer = (currPlayer+1) % plist.length;
 	}
@@ -254,6 +201,7 @@ public class Main {
 		
 	}
 	static void printBoard() {
+		ArrayList<String> info = new ArrayList<String>();
 		info.clear();
 		info.add("round: "+turns);
 		info.add("Board: ");
@@ -293,5 +241,60 @@ public class Main {
 			}
 		}
 		Collections.shuffle(deck);; //randomize deck
+	}
+	static void playerChoice() {
+		String c2="";
+		int choice = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("what do you want to do? 1. play card 2. show all known cards"));
+		a1:
+		if(choice==1) { // play card
+			String c1 = javax.swing.JOptionPane.showInputDialog("from hand/pile? c to cancel \n pile0-pile4 type the pile Number");
+			if(c1.equals("c")) {
+				break a1;
+			} else if(c1.equals("hand")) {
+				tempf = plist[0].hand;
+				c2 = javax.swing.JOptionPane.showInputDialog("position? 0-4, c to cancel ");
+				if(c2.equals("c")) {
+					break a1;
+				} else {
+					temppos = Integer.valueOf(c2);
+				}
+			} else if(Integer.valueOf(c1) != null) {
+				if(plist[0].pPiles.get(Integer.valueOf(c1)).size() != 0) {
+					tempf=plist[0].pPiles.get(Integer.valueOf(c1));
+				} else {
+					javax.swing.JOptionPane.showMessageDialog(null, "cant take from empty pile");
+					break a1;
+				}
+				
+			}
+			if(!(c1.equals("hand"))) {
+				temppos = tempf.size()-1;
+			}
+			if(c1.equals("c")) {
+				break a1;
+			}
+			String c3 =javax.swing.JOptionPane.showInputDialog("to pile? c to cancel \n mainpile0-mainpile3 pPile4-pPile7  type the pile number");
+			if(c3.equals("c")) {
+				break a1;
+			} else if(Integer.valueOf(c3) != null) {
+				if(Integer.valueOf(c3) > 3) {
+					tempt=plist[0].pPiles.get(Integer.valueOf(c3)-3);
+				} else {
+					tempt=mainPiles.get(Integer.valueOf(c3));
+				}
+			}
+			if(checkMove(tempf, temppos, tempt)) {
+				plist[0].playCard(tempf, temppos, tempt);
+				if(isToMainPile(tempt)) {
+					if(tempt.get(tempt.size()-1).id == 0) {
+						tempt.get(tempt.size()-1).id = tempt.size();
+					}
+				}
+			} else {
+				javax.swing.JOptionPane.showMessageDialog(null, "ILLEGAL MOVE");
+				doTurn();
+			}
+		} else if(choice==2) printBoard();
+		
 	}
 }
